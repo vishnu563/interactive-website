5667cec2c33dd10dbcbb70a6aaa1f9a423abcf5c
vishnu563 <vishnuprasanth050603@gmail.com>
2024-11-24 14:55:46 +0530

    Functionalities Added

---
+++ src/main.js
@@ -0,0 +1,30 @@
+function scrolltoOffer(){
+    const offerElement = document.getElementById('combo');
+    if(offerElement){
+        offerElement.scrollIntoView();
+    }
+}
+function scrolltoHome(){
+    const homeElement = document.getElementById('home');
+    if(homeElement){
+        homeElement.scrollIntoView();
+    }
+}
+function scrolltoStory(){
+    const storyElement = document.getElementById('philosophy')
+    if(storyElement){
+        storyElement.scrollIntoView();
+    }
+}
+function scrolltoMenu(){
+    const menuElement = document.getElementById('menu');
+    if(menuElement){
+        menuElement.scrollIntoView();
+    }
+}
+function scrolltoFind(){
+    const findElement = document.getElementById('find-us');
+    if(findElement){
+        findElement.scrollIntoView();
+    }
+}
