9b1aa839dbe5db7584e09669a5afd5b6fafe437e
vishnu563 <vishnuprasanth050603@gmail.com>
2024-11-25 20:10:16 +0530

    nav bar designed for mobile

--- src/main.js
+++ src/main.js
@@ -31,3 +31,26 @@ function scrolltoFind(){
 const currentYear = new Date().getFullYear();
 
 document.getElementById('year').textContent = currentYear;
+
+function handleSubmit(event){
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const feedBack = document.getElementById('feedback').value;
+    console.log("name: "+name,"email: "+email,"feedback: "+feedBack);
+    document.getElementById('name').value="";
+    document.getElementById('email').value="";
+    document.getElementById('feedback').value="";
+}
+
+let navList = document.getElementById('nav-list');
+navList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(navList.style.maxHeight == "0px"){
+        navList.style.maxHeight = "300px";
+    }
+    else{
+        navList.style.maxHeight = "0px";
+    }
+}
